290918efcfc703bc0a0ae76fc5c73e4d39d6e454
Kong <mgongzai@gmail.com>
2018-09-26 00:30:16 +0800

    add fetch day one scheule api

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 
+import axios from "axios";
+
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dayOneSchedule: []
+    };
   }
+
+  componentDidMount() {
+    this.fetchDayOneSchedule();
+  }
+
+  fetchDayOneSchedule = () => {
+    axios.get("/data/dayone.json").then(res => {
+      this.setState({ dayOneSchedule: res.data });
+    });
+  };
+
   render() {
+    console.log(this.state);
     return (
       <Button variant="contained" color="primary">
         Hello World
